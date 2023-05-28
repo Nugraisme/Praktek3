5781c62335689c52a028ec5eefdc29aeaa6a3aae
Nugra <60314686+Nugraisme@users.noreply.github.com>
2023-05-28 19:52:38 +0800

    Buat methode cetakInfo()

--- src/praktek3/Sepeda.java
+++ src/praktek3/Sepeda.java
@@ -12,4 +12,10 @@ public class Sepeda {
     String merk;
     String warna;
     Double harga;
+    
+    void cetakInfo(){
+        System.out.println("Merk \t : "+merk+"\n"+
+                "Warna \t : "+warna+"\n"+
+                "Harga \t : "+harga);
+    }
 }
