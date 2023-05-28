0061c587d5cb1d1a741ba037919da8fed1bc501a
Nugra <60314686+Nugraisme@users.noreply.github.com>
2023-05-28 20:44:29 +0800

    menambahkan setter and getter untuk SepedaMotor

--- src/praktek3/SepedaAksi.java
+++ src/praktek3/SepedaAksi.java
@@ -15,6 +15,7 @@ public class SepedaAksi {
        s.setWarna("Merah");
        s.setHarga(1000.0);
        
+       
        s.cetakInfo();
        
        System.out.print("Merknya    \t: ");
@@ -23,6 +24,14 @@ public class SepedaAksi {
        System.out.println(s.getWarna());
        System.out.print("Harganya   \t: ");
        System.out.println(s.getHarga());
-
+       
+       SepedaMotor sm = new SepedaMotor();
+       sm.setMerk("Hamaya");
+       sm.setWarna("Hitam");
+       sm.setHarga(10000.0);
+       sm.setJenisMotor("Listrik");
+       sm.cetakInfo();
+       System.out.println("Jenis \t: "+sm.getJenisMotor());
+       sm.throttle();
     }
 }
